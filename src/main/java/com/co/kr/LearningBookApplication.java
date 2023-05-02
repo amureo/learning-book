f04f5f2a8dcfa5da0ad81be227829aa8081e1242
amureo <wkeehd12@gmail.com>
2023-05-03 01:06:47 +0900

    create project

---
+++ src/main/java/com/co/kr/LearningBookApplication.java
@@ -0,0 +1,13 @@
+package com.co.kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningBookApplication.class, args);
+	}
+
+}
