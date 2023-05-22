6199eac1fdd433f4ce2af2f3f59619df7c28798d
amureo <wkeehd12@gmail.com>
2023-05-22 15:07:23 +0900

    .war 3

--- src/main/java/com/co/kr/LearningBookApplication.java
+++ src/main/java/com/co/kr/LearningBookApplication.java
@@ -7,16 +7,10 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearningBookApplication extends SpringBootServletInitializer{
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(LearningBookApplication.class);
-	}
-	
+public class LearningBookApplication {
+
 	@PostConstruct
 	public void started() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
